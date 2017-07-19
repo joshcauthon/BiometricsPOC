5a49273b9b2bf7d76ba1d1ba4c215720f3b9f455
Josh Cauthon <Josh.Cauthon@egov.com>
2017-07-19 15:48:28 -0500

    Added iOS WebViewRenderer

---
+++ BiometricsPOC/BiometricsPOC.iOS/WebViewWithArgsRenderer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using BiometricsPOC;
+using BiometricsPOC.iOS;
+using System.Diagnostics;
+
+[assembly: ExportRenderer(typeof(WebViewWithArgs), typeof(WebViewWithArgsRenderer))]
+namespace BiometricsPOC.iOS
+{
+    public class WebViewWithArgsRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Delegate = new WebViewDelegate(WebViewWithArgs);
+            }
+        }
+
+        public WebViewWithArgs WebViewWithArgs
+        {
+            get { return Element as WebViewWithArgs; }
+        }
+    }
+
+    internal class WebViewDelegate : UIWebViewDelegate
+    {
+        private WebViewWithArgs _webViewWithArgs;
+
+        public WebViewDelegate(WebViewWithArgs webViewWithArgs)
+        {
+            _webViewWithArgs = webViewWithArgs;
+        }
+
+        [Export("webView:shouldStartLoadWithRequest:navigationType:")]
+        public override bool ShouldStartLoad(UIWebView webView, NSUrlRequest request, UIWebViewNavigationType navigationType)
+        {
+            webView.UserInteractionEnabled = true;
+
+            _webViewWithArgs.OnNavigating(new WebViewNavigationEventArgs
+            {
+                Url = request.Url.AbsoluteString
+            });
+
+            return true;
+        }
+
+        public override void LoadStarted(UIWebView webView)
+        {
+
+        }
+
+        public override void LoadFailed(UIWebView webView, NSError error)
+        {
+            Debug.WriteLine($"Error: {error.ToString()}");
+        }
+
+        [Export("webViewDidFinishLoad:")]
+        public override void LoadingFinished(UIWebView webView)
+        {
+            webView.UserInteractionEnabled = true;
+
+            _webViewWithArgs.OnNavigated(new WebViewNavigatedEventArgs
+            {
+                Url = webView.Request.Url.AbsoluteString
+            });
+        }
+    }
+}
